b6b308cefa0f6413d36373ba2dcc409c3dc2d941
er-rizwan <errizwan@hotmail.com>
2024-07-27 08:00:20 +0530

    Added CRUD methods

--- CoreWeb/Controllers/HomeController.cs
+++ CoreWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using CoreWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CoreWeb.Controllers
@@ -18,13 +19,116 @@ namespace CoreWeb.Controllers
         {
 			this.studentDB = studentDB;
 		}
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
 		{
-			var studentdata = studentDB.Students.ToList();
+			var studentdata = await studentDB.Students.ToListAsync();
 				return View(studentdata);
 		}
 
-		public IActionResult Privacy()
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(Student student)
+		{
+			if (ModelState.IsValid)
+			{
+			await studentDB.Students.AddAsync(student);
+				await studentDB.SaveChangesAsync();
+				TempData["create_msg"] = "Successfully Created.";
+                return RedirectToAction("Index","Home");
+			}
+				TempData["Create_msg"] = "Unseccessful. Try again!";
+            return View(student);
+		     }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+				if (id == null || studentDB.Students==null) {
+				 return NotFound();
+				}
+				var studentdata = await studentDB.Students.FirstOrDefaultAsync(x => x.Id == id);
+				if (studentdata == null)
+				{
+                    return NotFound();
+                }
+			 return View(studentdata);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || studentDB.Students == null)
+            {
+                return NotFound();
+            }
+
+            var studentdata = await studentDB.Students.FindAsync(id);
+
+			if (studentdata == null) {
+				return NotFound();
+			}
+
+			return View(studentdata);
+        }
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(int? id,Student student)
+		{
+			if (!ModelState.IsValid)
+			{
+                return View(student);
+			}
+
+			if (student.Id != id) {
+				return NotFound();
+			}
+
+            studentDB.Students.Update(student);
+            await studentDB.SaveChangesAsync();
+            TempData["edit_msg"] = "Successfully Updated.";
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || studentDB.Students == null)
+            {
+                return NotFound();
+            }
+            var studentdata = await studentDB.Students.FirstOrDefaultAsync(x => x.Id == id);
+            if (studentdata == null)
+            {
+                return NotFound();
+            }
+            return View(studentdata);
+        }
+
+		[HttpPost,ActionName("Delete")]
+		public async Task<IActionResult> DeleteConfirm(int? id)
+		{
+            if (id == null || studentDB.Students == null)
+            {
+                return NotFound();
+            }
+
+            var studentdata = await studentDB.Students.FindAsync(id);
+
+            if (studentdata == null)
+            {
+                return NotFound();
+            }
+
+			studentDB.Students.Remove(studentdata);
+			await studentDB.SaveChangesAsync();
+            TempData["delete_msg"] = "Successfully Deleted.";
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Privacy()
 		{
 			return View();
 		}
--- CoreWeb/Models/Student.cs
+++ CoreWeb/Models/Student.cs
@@ -6,13 +6,26 @@ namespace CoreWeb.Models
 	public class Student
 	{
         [Key]
+		[Required]
         public int Id { get; set; }
+
+
         [Column("StudentName"),DataType("varchar(100)")]
         public string Name { get; set; }
+
+
 		[Column("StudentGender"), DataType("varchar(10)")]
+		[Required]
 		public string Gender { get; set; }
-		[Column("StudentAge"), DataType("int")]
+
+
+		[Column("StudentAge"), DataType("int")]	
+		[Required]
 		public int Age { get; set; }
+
+
+		[Column("Standard"), DataType("int")]
+		[Required]
 		public int Standard { get; set; }
 	}
 }
